d0e1492e557a4059b4efc65482fc8faf0d05785a
YuZhong-Chen <yuzhong1214@gmail.com>
2025-11-30 19:41:55 +0800

    feat: Refactor easy_handeye2 launch files to use a consistent argument declaration.

--- src/easy_handeye2/easy_handeye2/launch/evaluate.launch.py
+++ src/easy_handeye2/easy_handeye2/launch/evaluate.launch.py
@@ -3,18 +3,31 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        "name",
+        default_value="eye_on_base_calibration",
+        description="Name of the calibration.",
+    ),
+]
+
 
 def generate_launch_description():
-    arg_name = DeclareLaunchArgument('name')
+    arg_name = DeclareLaunchArgument("name")
 
-    handeye_rqt_evaluator = Node(package='easy_handeye2', executable='rqt_evaluator.py',
-                                  name='handeye_rqt_evaluator',
-                                  # arguments=['--ros-args', '--log-level', 'debug'],
-                                  parameters=[{
-                                      'name': LaunchConfiguration('name'),
-                                  }])
+    handeye_rqt_evaluator = Node(
+        package="easy_handeye2",
+        executable="rqt_evaluator.py",
+        name="handeye_rqt_evaluator",
+        # arguments=['--ros-args', '--log-level', 'debug'],
+        parameters=[
+            {
+                "name": LaunchConfiguration("name"),
+            }
+        ],
+    )
 
-    return LaunchDescription([
-        arg_name,
-        handeye_rqt_evaluator,
-    ])
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(arg_name)
+    ld.add_action(handeye_rqt_evaluator)
+    return ld
--- src/easy_handeye2/easy_handeye2/launch/publish.launch.py
+++ src/easy_handeye2/easy_handeye2/launch/publish.launch.py
@@ -3,15 +3,30 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        "name",
+        default_value="eye_on_base_calibration",
+        description="Name of the calibration.",
+    ),
+]
+
 
 def generate_launch_description():
-    arg_name = DeclareLaunchArgument('name')
+    arg_name = DeclareLaunchArgument("name")
 
-    handeye_publisher = Node(package='easy_handeye2', executable='handeye_publisher', name='handeye_publisher', parameters=[{
-        'name': LaunchConfiguration('name'),
-    }])
+    handeye_publisher = Node(
+        package="easy_handeye2",
+        executable="handeye_publisher",
+        name="handeye_publisher",
+        parameters=[
+            {
+                "name": LaunchConfiguration("name"),
+            }
+        ],
+    )
 
-    return LaunchDescription([
-        arg_name,
-        handeye_publisher,
-    ])
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(arg_name)
+    ld.add_action(handeye_publisher)
+    return ld
