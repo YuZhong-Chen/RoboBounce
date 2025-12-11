61aaf01ac92b45caf6022025bd37f57a91f4b9d1
YuZhong-Chen <yuzhong1214@gmail.com>
2025-12-11 12:42:58 +0800

    feat: Remove default calibration files and use the file extracted from robot.

--- scripts/extract_ur_calibration.sh
+++ scripts/extract_ur_calibration.sh
@@ -7,4 +7,4 @@ ORIGINAL_DIR="$(pwd)"
 
 ros2 launch ur_calibration calibration_correction.launch.py \
     robot_ip:=192.168.56.101 \
-    target_filename:="/home/user/RoboBounce/config/ur5_calibration.yaml"
+    target_filename:="/home/user/RoboBounce/src/Universal_Robots_ROS2_Description/config/ur5/default_kinematics.yaml"
--- scripts/ur_driver_bringup.sh
+++ scripts/ur_driver_bringup.sh
@@ -3,5 +3,4 @@
 ros2 launch ur_robot_driver ur_control.launch.py \
     ur_type:=ur5 \
     robot_ip:=192.168.56.101 \
-    launch_rviz:=true \
-    kinematics_params_file:=/home/user/RoboBounce/config/ur5_calibration.yaml
+    launch_rviz:=true
