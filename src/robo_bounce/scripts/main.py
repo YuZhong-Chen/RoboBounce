cdeca48e26a26258aba13dd4ae8b470dc12684cf
YuZhong-Chen <yuzhong1214@gmail.com>
2025-12-11 21:32:29 +0800

    feat: Create RoboBounce main control node.
    Copy from the ur_servo_control node.

---
+++ src/robo_bounce/scripts/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rclpy
+import subprocess
+from robo_bounce.robo_bounce import RoboBounce
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    robo_bounce_node = RoboBounce()
+
+    try:
+        rclpy.spin(robo_bounce_node)
+    except KeyboardInterrupt:
+        # Publish stop command on shutdown
+        robo_bounce_node.get_logger().info("Shutdown RoboBounce node...")
+        topic_name = "/forward_velocity_controller/commands"
+        msg_type = "std_msgs/msg/Float64MultiArray"
+        msg_content = "{data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}"
+        cmd = ["ros2", "topic", "pub", "-t", "3", topic_name, msg_type, msg_content]
+        subprocess.run(cmd)
+    finally:
+        robo_bounce_node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
