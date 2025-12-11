5a06cc1624a2789a989029e6dd9e2627b1750a0c
YuZhong-Chen <yuzhong1214@gmail.com>
2025-12-11 12:25:13 +0800

    feat: Add configuration for UR5 calibration.

---
+++ scripts/extract_ur_calibration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Get the directory of this script.
+# Reference: https://stackoverflow.com/q/59895
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+ORIGINAL_DIR="$(pwd)"
+
+ros2 launch ur_calibration calibration_correction.launch.py \
+    robot_ip:=192.168.56.101 \
+    target_filename:="/home/user/RoboBounce/config/ur5_calibration.yaml"
