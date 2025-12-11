a2a730b006b8c5d698c1129b7e4adb7e3227dedf
YuZhong-Chen <yuzhong1214@gmail.com>
2025-12-11 21:53:44 +0800

    feat: Add parameter in realsense_bringup scripts.

--- scripts/realsense_bringup.sh
+++ scripts/realsense_bringup.sh
@@ -5,5 +5,13 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 ORIGINAL_DIR="$(pwd)"
 
+IS_CALIBRATION=false
+
 # Launch the realsense
-ros2 launch realsense_launch realsense.launch.py
+if [ "$IS_CALIBRATION" = false ] ; then
+    ros2 launch realsense_launch realsense.launch.py
+else
+    ros2 launch realsense_launch realsense.launch.py \
+        enable_depth:=false \
+        rgb_profile:=1920x1080x8
+fi
